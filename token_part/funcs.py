da305d7d969cb22edd69799ebf04bb5ffe2abda2
SpongeFrogy <droidb1poc@gmail.com>
2023-04-07 13:30:35 +0300

    dub reset

--- token_part/funcs.py
+++ token_part/funcs.py
@@ -19,20 +19,41 @@ def numberDict(dataframe, column_name):
 class Dataset:
     def __init__(self, path_to_csv):
         dataframe = pd.read_csv(path_to_csv, sep=',', index_col=0)
+        dataframe = dataframe.rename(columns={'_cell_length_a': 'a', '_cell_length_b': 'b', '_cell_length_c': 'c', '_cell_angle_alpha':'alpha', '_cell_angle_beta':'beta','_cell_angle_gamma': 'gamma','_cell_volume':'cell volume','_symmetry_space_group_name_Hall':'space group'})
         self.df = dataframe
         self.columns = dataframe.columns
+        self.token_dicts = None
         # self.tokenDictFunc = dict(zip(list(dataframe.columns), 
         #                      list((lambda val: numberDict(dataframe, _col)[val]) for _col in dataframe.columns)))
-        self.token_dicts = dict(zip(list(dataframe.columns), [None]*len(dataframe.columns)))
+    def prepare_like_03_04(self):
+        """
+        1. drop duplicates by mofkey 
+        2. tokenize with number of unique values
+        3. linear scale in [0;1] (float64)
+        4. drop mofkey/mofid
+        """
+        # 1
+        mofkeyValues = self.df.mofkey.value_counts()[self.df.mofkey.value_counts() > 1]
+        for mkey in mofkeyValues.index:
+            self.df = self.df.drop(index=self.df.loc[self.df.mofkey==mkey].index)
+        # 2
+        self.tokenize_df()
+        # 3
+        self.scale_data(preprocessing.MinMaxScaler())
+        # 4
+        self.df = self.df.drop(['mofid', 'mofkey'], axis=1)
+
     
     # preprocessing dataset
     
-    # tokenaze       
+    # tokenize       
     def tokenize_column(self, column, token_func=None):
         """
         Get column name in 'Dataset.df' and tokenize with 'token_func'.
         Numbers unique val if 'token_func=None'.
         """
+        if self.token_dicts == None:
+            self.token_dicts = dict(zip(list(self.df.columns), [None]*len(self.df.columns)))
         if token_func == None:
             token_func = numberDict(self.df, column)
             if self.token_dicts[column] == None:
@@ -44,6 +65,9 @@ class Dataset:
         Do tokenize_column(self, column, token_func=None) for columns .
         Numbers unique val if 'token_funcs=None'.
         """
+        if self.token_dicts == None:
+            self.token_dicts = dict(zip(list(self.df.columns), [None]*len(self.df.columns)))
+        
         if token_funcs == None:
             token_funcs = [None]*len(self.columns)
         for args in zip(self.columns, token_funcs):
@@ -51,8 +75,11 @@ class Dataset:
 
     #set scale
     def scale_data(self, scaler=None):
+        """
+        Works with Scaler, Normalizer from sklearn, if scaler=None, then MinMaxScaler()
+        """
         if scaler == None:
-            scaler = preprocessing.StandardScaler()
+            scaler = preprocessing.MinMaxScaler()
         dataScaledArr = scaler.fit_transform(self.df)
         newVals = scaler.transform(pd.DataFrame([d.values() for d in self.token_dicts.values()]).T)
         for i, col in enumerate(self.token_dicts.keys()):
